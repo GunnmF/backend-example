85c0844d84529de0013d7cecc607f9cd2bb91801
fanchen <moumou.v1@foxmail.com>
2023-04-27 16:42:36 +0800

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: moumou.v1@foxmail.com
  * @Date: 2023-03-29 16:09:19
- * @LastEditTime: 2023-04-25 21:48:27
+ * @LastEditTime: 2023-04-27 16:42:28
  * @LastEditors: moumou.v1@foxmail.com
  */
 import { Injectable } from '@nestjs/common';
@@ -10,6 +10,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '这是服务端';
   }
 }
